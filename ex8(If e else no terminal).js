11721ceb0baf7af332a0fee8fab887c946317624
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-06 20:19:03 -0300

    first commit

---
+++ ex8(If e else no terminal).js
@@ -0,0 +1,13 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // condição simples, sem o 'else'
+    console.log(`Ei você ultrapassou a velocidade, multado!!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+var pais = 'Brasil'
+console.log(`Vivendo no pais: ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Voce é Brasileiro!')
+}else {
+    console.log('Voce é Estrangeiro!')
+}
